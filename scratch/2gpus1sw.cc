7c2c192bfc3c37aaa92da0aec277d3d9da18798d
Jacqueline Yujia Wang <wangyj05@mit.edu>
2026-06-10 07:04:39 -0400

    file sync

--- scratch/2gpus1sw.cc
+++ scratch/2gpus1sw.cc
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 
 		constexpr int N_NODES = 2;
 		constexpr int N_CHUNKS = 1;
-        constexpr int INPUT_BYTES = (1 << 10);
+        constexpr int INPUT_BYTES = (1 << 20);
 		int CHUNK_SIZE = (INPUT_BYTES / N_CHUNKS) / DataType::GetSizeBytes(DataType::INT32);
         constexpr bool CORRECTNESS_CHECK = true;
 
